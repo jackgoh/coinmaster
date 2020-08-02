ed87c86d43a7242af997a9044b659ae5ec9289d2
Jack Goh <jackgoh0@gmail.com>
2020-08-02 11:22:02 +0800

    Remove socketio and gui

--- index.js
+++ index.js
@@ -6,76 +6,59 @@ const csv = require("csv-parser");
 const accountFile = ".account.csv";
 var myArgs = process.argv.slice(2);
 let accounts = [];
-(async () => {
-  const server = require('http').createServer();
-  const io = require('socket.io')(server);
-  if(!fs.existsSync("data")){
-    fs.mkdirSync("data");
-  }
-  io.on('connection', client => {
-    client.on('connected', data => {
-      io.emit("connected", true)
-    });
-    client.on('disconnect', () => {
-      /* … */
-    });
-    client.emit("accounts", accounts);
-    client.emit("connected", true)
-  });
-  portfinder.getPortPromise({
-    port: 3001,
-    stopPort: 3099
-  }).then(port => {
 
-    console.log("start socket server at port ", port)
-    server.listen(port);
-  }).catch(err => {
-    console.log("No port available for start up socker server".red);
-    console.log(err)
-  })
+const server = require('http').createServer();
+if (!fs.existsSync("data")) {
+  fs.mkdirSync("data");
+}
+portfinder.getPortPromise({
+  port: 3001,
+  stopPort: 3099
+}).then(port => {
 
-  if (fs.existsSync(accountFile) && myArgs.length > 0) {
-    accounts = []
+  console.log("start socket server at port ", port)
+  server.listen(port);
+}).catch(err => {
+  console.log("No port available for start up socker server".red);
+  console.log(err)
+})
 
-    fs.createReadStream(accountFile)
-      .pipe(csv())
-      .on('data', (data) => accounts.push(data))
-      .on('end', async () => {
-        console.log(accounts);
-        var index = myArgs[0] || "-1";
-        for (const account of accounts.filter(x => x.ID === index || index == "all")) {
-          try {
-            if (account.EMAIL[0] === "#" && isNaN(index)) continue;
-            console.log("PLAY AS: ", account)
-            var cm = new CoinMaster({
-              // sycnTarget: account.SYNC_TARGET,
-              userId: account.USER_ID,
-              fbToken: account.FB_TOKEN,
-              deviceId: account.DEVICE_ID,
-              onData: (d) => {
-                io.emit(d);
-              }
-            });
+if (fs.existsSync(accountFile) && myArgs.length > 0) {
+  accounts = []
 
-            const balance = await cm.play()
-          } catch (err) {
-            console.error(err)
-          }
+  fs.createReadStream(accountFile)
+    .pipe(csv())
+    .on('data', (data) => accounts.push(data))
+    .on('end', async () => {
+      console.log(accounts);
+      var index = myArgs[0] || "-1";
+      for (const account of accounts.filter(x => x.ID === index || index == "all")) {
+        try {
+          if (account.EMAIL[0] === "#" && isNaN(index)) continue;
+          console.log("PLAY AS: ", account)
+          var cm = new CoinMaster({
+            // sycnTarget: account.SYNC_TARGET,
+            userId: account.USER_ID,
+            fbToken: account.FB_TOKEN,
+            deviceId: account.DEVICE_ID,
+            onData: (d) => {
+            }
+          });
+
+          const balance = cm.play()
+        } catch (err) {
+          console.error(err)
         }
-        process.exit(0);
-      });
-
-    console.log("Multiple play use account file");
-  } else {
-    // await new Promise((resolve) => setTimeout(resolve, 15000))
-    var cm = new CoinMaster({
-      onData: (d) => {
-        io.emit(d);
       }
+      process.exit(0);
     });
-    //const friend = await cm.getFriend("A_cj7ui7x6m00imtms1r9sxw8b0");
-    // console.log("friends", friend);
 
-    const balance = await cm.play();
-  }
-})();
+  console.log("Multiple play use account file");
+} else {
+  var cm = new CoinMaster({
+    onData: (d) => {
+    }
+  });
+
+  const balance = cm.play();
+}
